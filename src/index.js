e28fda31305f97abcd76d5c40ab66cb8a7596c0a
chantie84 <cc.crompvoets@gmail.com>
2025-02-11 12:11:59 +0100

    updated context

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ function generateRecipe(event) {
   let apiKey = "e1e10606109dc39aa96a4o5b4facfd3t";
   let prompt = `User instructions: Generate a pistachio recipe ${instructionsInput.value}`;
   let context =
-    "You are a excellent expert in baking or cooking with pistachio and love to write short recipes. Your mission is to generate a pistachio recipe in basic HTML and seperate each line with a <br />. Make sure to follow the user instructions.The title of the recipe, ingredients and instructions is in italic. Sign the recipe with 'SheCodes AI' insight a <strong> element at the end of the recipe";
+    "You are a excellent expert in baking or cooking with pistachio and love to write short recipes. Your mission is to generate a 10 lines pistachio recipe in basic HTML and seperate each line with a <br />. Make sure to follow the user instructions.The title of the recipe, ingredients and instructions is in italic. Sign the recipe with 'SheCodes AI' insight a <strong> element at the end of the recipe";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   console.log("Generating Recipe..");
