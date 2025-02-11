e6306576a398a768daac166fa08f6da5f6bb48ef
chantie84 <cc.crompvoets@gmail.com>
2025-02-11 14:08:23 +0100

    Improved the User Experience and removed the console log

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 function displayRecipe(response) {
-  console.log("Recipe generated");
   new Typewriter("#pistachio-recipe", {
     strings: response.data.answer,
     autoStart: true,
@@ -19,12 +18,12 @@ function generateRecipe(event) {
     "You are a excellent expert in baking or cooking with pistachio and love to write short recipes. Your mission is to generate a 10 lines pistachio recipe in basic HTML and seperate each line with a <br />. Make sure to follow the user instructions.The title of the recipe, ingredients and instructions is in italic. Sign the recipe with 'SheCodes AI' insight a <strong> element at the end of the recipe";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  console.log("Generating Recipe..");
-  console.log(`Prompt ${prompt}`);
-  console.log(`Context ${context}`);
+  let recipeElement = document.querySelector("#pistachio-recipe");
+  recipeElement.classList.remove("hidden");
+  recipeElement.innerHTML = `<div class="generating">🥰 Generating a pistachio recipe about ${instructionsInput.value}</div>`;
 
   axios.get(apiUrl).then(displayRecipe);
 }
 
-let receiptFormElement = document.querySelector("#receipt-generator-form");
-receiptFormElement.addEventListener("submit", generateRecipe);
+let recipeFormElement = document.querySelector("#recipe-generator-form");
+recipeFormElement.addEventListener("submit", generateRecipe);
